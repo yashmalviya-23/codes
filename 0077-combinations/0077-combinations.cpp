6e150f43c5a03a6fcbe08e2b4058696a77cfba8b
Yash Malviya <yashmalviya2309@gmail.com>
2026-08-05 17:39:20 +0530

    Time: 74 ms (37.55%), Space: 82.4 MB (46.85%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(int start, int n, int k, vector<int>& temp) {
+        if (temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            temp.push_back(i);
+            solve(i + 1, n, k, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        solve(1, n, k, temp);
+        return ans;
+    }
+};
