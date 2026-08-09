703c5a8475c9b3085d638a785b5a37f9fc0eedcb
Yash Malviya <yashmalviya2309@gmail.com>
2026-08-09 21:58:47 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.83%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        if(target > nums[nums.size()-1]){
+            return nums.size();
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] >= target){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+};
