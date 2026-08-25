a53c5988f98dd973c2d8f495508ec019abcb1731
Yash Malviya <yashmalviya2309@gmail.com>
2026-08-25 19:37:52 +0530

    Time: 0 ms (100%), Space: 9.1 MB (95.89%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+
+        for(char c : s)
+            ans ^= c;
+
+        for(char c : t)
+            ans ^= c;
+
+        return ans;
+    }
+};
