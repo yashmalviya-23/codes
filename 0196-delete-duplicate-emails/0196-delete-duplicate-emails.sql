43dc67571dfb37ad7faab8df986673cc85f249b1
Yash Malviya <yashmalviya2309@gmail.com>
2026-09-01 23:20:05 +0530

    Time: 2847 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -4,7 +4,3 @@ FROM Person p1
 JOIN Person p2
 ON p1.email = p2.email
 AND p1.id > p2.id;
-
--- Synced seamlessly with LeetHub Pro
--- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
--- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
