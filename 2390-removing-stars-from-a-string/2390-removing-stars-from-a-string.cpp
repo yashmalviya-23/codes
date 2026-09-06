dfd41c392ddfe68b061d896ce671ac53f860ac63
Yash Malviya <yashmalviya2309@gmail.com>
2026-09-06 17:16:37 +0530

    Time: 22 ms (38.09%), Space: 28.3 MB (77.36%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string ans;
-        for(int i{0};i<s.length();i++){
-            if(s[i]!='*'){
-                ans.push_back(s[i]);
-            }
-            else{
-                ans.pop_back();
+        string res;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                if (res.size() != 0) {
+                    res.pop_back();
+                }
+            } else {
+                res.push_back(s[i]);
             }
         }
-        return ans;
+        return res;
     }
 };
